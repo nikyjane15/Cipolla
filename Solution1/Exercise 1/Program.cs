a3f4e9ed2f802eab4995d37fdd5b33c2583024f5
Nikyjane15 <nikyjane15@gmail.com>
2017-10-12 14:10:00 +0200

    Try to add files

---
+++ Solution1/Exercise 1/Program.cs
@@ -0,0 +1,12 @@
+﻿//our first project, wow!!
+namespace HelloWorld
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello C# Worlddddd");
+    }
+}
+}
+
